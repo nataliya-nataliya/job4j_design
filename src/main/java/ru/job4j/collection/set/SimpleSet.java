c2329896cab1d36ef6fb4909107c361b1e09bb1f
Nataliya-nataliya <nataliya.chest@gmail.com>
2021-12-25 19:40:25 +0300

    fix - метод add

--- src/main/java/ru/job4j/collection/set/SimpleSet.java
+++ src/main/java/ru/job4j/collection/set/SimpleSet.java
@@ -11,8 +11,8 @@ public class SimpleSet<T> implements  Set<T> {
 
     @Override
     public boolean add(T value) {
-        boolean rsl = false;
-        if (!contains(value)) {
+        boolean rsl = !contains(value);
+        if (rsl) {
             set.add(value);
             rsl = true;
         }
