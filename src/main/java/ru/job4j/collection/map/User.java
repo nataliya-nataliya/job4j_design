b050e6e5d0ee4a0539f01e94f1067494bb6a6cdd
Nataliya-nataliya <nataliya.chest@gmail.com>
2021-12-26 20:17:49 +0300

    fix - геттеры и сеттеры

--- src/main/java/ru/job4j/collection/map/User.java
+++ src/main/java/ru/job4j/collection/map/User.java
@@ -12,4 +12,28 @@ public class User {
         this.children = children;
         this.birthday = birthday;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public void setChildren(int children) {
+        this.children = children;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Calendar birthday) {
+        this.birthday = birthday;
+    }
 }
