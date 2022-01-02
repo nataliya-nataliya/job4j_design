39a6a9bbfe64387889a07add080b057881b7df6d
Nataliya-nataliya <nataliya.chest@gmail.com>
2022-01-02 14:34:59 +0300

    2. Без переопределения equals и hashCode [#1005]

--- src/main/java/ru/job4j/collection/map/User.java
+++ src/main/java/ru/job4j/collection/map/User.java
@@ -1,6 +1,9 @@
 package ru.job4j.collection.map;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
     private String name;
@@ -35,5 +38,14 @@ public class User {
 
     public void setBirthday(Calendar birthday) {
         this.birthday = birthday;
+    }
+
+    public static void main(String[] args) {
+        User user1 = new User("Anna", 1, new GregorianCalendar(1985, Calendar.JANUARY, 5));
+        User user2 = new User("Anna", 1, new GregorianCalendar(1985, Calendar.JANUARY, 5));
+        Map<User, Object> map = new HashMap<>();
+        map.put(user1, new Object());
+        map.put(user2, new Object());
+        System.out.println(map);
     }
 }
