211949f290fecbfd3d99deddd0b6ec7c0b800f35
Nataliya-nataliya <nataliya.chest@gmail.com>
2022-01-21 23:25:44 +0300

    1. Создать элементарную структуру дерева [#1711]

---
+++ src/main/java/ru/job4j/collection/tree/Tree.java
@@ -0,0 +1,46 @@
+package ru.job4j.collection.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+public interface Tree<E> {
+    boolean add(E parent, E child);
+
+    Optional<Node<E>> findBy(E value);
+
+    class Node<E> {
+        final E value;
+        final List<Node<E>> children = new ArrayList<>();
+
+        public Node(E value) {
+            this.value = value;
+        }
+
+        public E getValue() {
+            return value;
+        }
+
+        public List<Node<E>> getChildren() {
+            return children;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            Node<?> node = (Node<?>) o;
+            return Objects.equals(value, node.value) && Objects.equals(children, node.children);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(value, children);
+        }
+    }
+}
