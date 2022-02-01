7f9e6352a6cae3a0d445eef272afc2955d7ce5b1
Nataliya-nataliya <nataliya.chest@gmail.com>
2022-02-02 00:40:03 +0300

    изменение equals c константой

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -9,10 +9,10 @@ public class Analysis {
             boolean on = true;
             for (String line = read.readLine(); line != null; line = read.readLine()) {
                 String[] s = line.split(" ");
-                if (!on && (s[0].equals("200")  || s[0].equals("300"))) {
+                if (!on && ("200".equals(s[0])  || "300".equals(s[0]))) {
                     out.println(s[1]);
                     on = true;
-                } else if (on && (s[0].equals("400") || s[0].equals("500"))) {
+                } else if (on && ("400".equals(s[0]) || "500".equals(s[0]))) {
                         out.print(s[1] + ";");
                         on = false;
                 }
