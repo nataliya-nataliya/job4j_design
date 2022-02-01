12c4c68a434b4872704157c6888aa2eda0015504
Nataliya-nataliya <nataliya.chest@gmail.com>
2022-02-01 22:00:49 +0300

    Замена contains на equals и сокращение

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -8,22 +8,13 @@ public class Analysis {
              PrintStream out = new PrintStream(target)) {
             boolean on = true;
             for (String line = read.readLine(); line != null; line = read.readLine()) {
-                if (line.contains("200") || line.contains("300")) {
-                    if (on) {
-                        continue;
-                    } else {
-                        String[] s = line.split(" ");
-                        out.println(s[1]);
-                        on = true;
-                    }
-                } else if (line.contains("400") || line.contains("500")) {
-                    if (on) {
-                        String[] s = line.split(" ");
+                String[] s = line.split(" ");
+                if (!on && (s[0].equals("200")  || s[0].equals("300"))) {
+                    out.println(s[1]);
+                    on = true;
+                } else if (on && (s[0].equals("400") || s[0].equals("500"))) {
                         out.print(s[1] + ";");
                         on = false;
-                    } else {
-                        continue;
-                    }
                 }
             }
         } catch (IOException e) {
