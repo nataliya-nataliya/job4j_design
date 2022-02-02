4b33e0c63155ddaeb5b182884f6e16c63a4d6e27
Nataliya-nataliya <nataliya.chest@gmail.com>
2022-02-02 22:33:03 +0300

    лишние скобки

--- src/main/java/ru/job4j/io/SearchFiles.java
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -27,7 +27,7 @@ public class SearchFiles implements FileVisitor<Path> {
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        if ((condition.test(file))) {
+        if (condition.test(file)) {
             list.add(file);
         }
         return  CONTINUE;
