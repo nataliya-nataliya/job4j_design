4bc6b2b3b25596195bfce1c3ccc857d3405cb99a
Nataliya-nataliya <nataliya.chest@gmail.com>
2022-02-10 00:16:44 +0300

    fix - добавлена валидация аргументов

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,12 +10,16 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        if (args.length == 0 || args.length == 1) {
-            throw new IllegalArgumentException("Введите через пробел абсолютный путь папки поиска и расширение файла");
+        File file = new File(args[0]);
+        if (args.length != 2
+        || !file.exists() || !file.isDirectory() || !file.isAbsolute()
+        || !(".").equals(args[1].substring(0, 1))) {
+            throw new IllegalArgumentException("Введите через пробел абсолютный путь папки поиска "
+                    + "и расширение файла с точкой");
         }
-        Path start = Paths.get(args[0]);
-        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
-    }
+            Path start = Paths.get(args[0]);
+            search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
+        }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
