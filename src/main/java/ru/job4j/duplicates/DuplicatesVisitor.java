cf7eaeeba8ed23145336d8f27277c356ae363603
Nataliya-nataliya <nataliya.chest@gmail.com>
2022-02-09 22:46:38 +0300

    fix- вынесен отдельный метод печати абсолютного пути

--- src/main/java/ru/job4j/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/duplicates/DuplicatesVisitor.java
@@ -14,23 +14,25 @@ import java.util.Map;
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
     private final Map<FileProperty, List<Path>> map = new HashMap<>();
 
-    /**
-     * Проверка на размер ArrayList добавлена, чтобы вывести абсолютный путь первого файла,
-     * у которого найден дубликат
-     */
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         FileProperty fileProperty = new FileProperty(attrs.size(), file.getFileName().toString());
         if (map.containsKey(fileProperty)) {
-            if (map.get(fileProperty).size() == 1) {
-                System.out.println(map.get(fileProperty).get(0).toAbsolutePath());
-            }
+            printAbsolutePath(fileProperty, file);
             map.get(fileProperty).add(file.toAbsolutePath());
-            System.out.println(file.toAbsolutePath());
         } else {
             map.put(fileProperty, new ArrayList<>());
             map.get(fileProperty).add(file.toAbsolutePath());
         }
         return super.visitFile(file, attrs);
+    }
+
+    public void printAbsolutePath(FileProperty fp, Path path) {
+        if (map.get(fp).size() == 1) {
+            System.out.println(map.get(fp).get(0).toAbsolutePath());
+            System.out.println(path.toAbsolutePath());
+        } else {
+            System.out.println(path.toAbsolutePath());
+        }
     }
 }
