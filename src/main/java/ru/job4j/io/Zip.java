8b92fe242b685b0eb3686d05034a1ab2dc650e47
Nataliya-nataliya <nataliya.chest@gmail.com>
2022-02-17 22:09:21 +0300

    fix добавлен новый метод валидации

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -8,6 +8,19 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
+    private static void validation(String[] array, Path path) {
+        if (array.length != 3) {
+            throw  new IllegalArgumentException("Введите ключи и их значения в виде -key=value"
+                    + System.lineSeparator()
+                    + "-d - папка, которую нужно архивировать"
+                    + System.lineSeparator()
+                    + "-e - файлы, которые надо исключить (расширение с точкой)"
+                    + System.lineSeparator()
+                    + "-o - название архива с расширением .zip");
+        } else if (!path.toFile().isDirectory()) {
+            throw new IllegalArgumentException("Не существует папки, которую нужно архивировать");
+        }
+    }
 
     public void packFiles(List<Path> sources, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
@@ -22,35 +35,11 @@ public class Zip {
         }
     }
 
-    public void packSingleFile(File source, File target) {
-        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(
-                new FileOutputStream(target)))) {
-            zip.putNextEntry(new ZipEntry(source.getPath()));
-            try (BufferedInputStream out = new BufferedInputStream(
-                    new FileInputStream(source))) {
-                zip.write(out.readAllBytes());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         ArgsName argsName = ArgsName.of(args);
         File o = new File(argsName.get("o"));
-        if (args.length != 3) {
-            throw  new IllegalArgumentException("Введите ключи и их значения в виде -key=value"
-            + System.lineSeparator()
-            + "-d - папка, которую нужно архивировать"
-            + System.lineSeparator()
-            + "-e - файлы, которые надо исключить (расширение с точкой)"
-            + System.lineSeparator()
-            + "-o - название архива с расширением .zip");
-        }
         Path d = Paths.get(argsName.get("d"));
-        if (!d.toFile().isDirectory()) {
-            throw new IllegalArgumentException("Не существует папки, которую нужно архивировать");
-        }
+        validation(args, d);
         Zip zip = new Zip();
         List<Path> list = Search.search(d, p -> !p.toFile().getName().endsWith(argsName.get("e")));
         zip.packFiles(list, o);
