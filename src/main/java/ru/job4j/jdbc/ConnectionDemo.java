2d4073bf0338fd4c18085638b4fae94617151cad
Nataliya-nataliya <nataliya.chest@gmail.com>
2022-04-24 14:01:17 +0300

    add driver from file

--- src/main/java/ru/job4j/jdbc/ConnectionDemo.java
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -9,12 +9,13 @@ import java.sql.SQLException;
 
 public class ConnectionDemo {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        Class.forName("org.postgresql.Driver");
         Config config = new Config("./app.properties");
         config.load();
+        String driver = config.value("hibernate.connection.driver_class");
         String url = config.value("hibernate.connection.url");
         String login = config.value("hibernate.connection.username");
         String password = config.value("hibernate.connection.password");
+        Class.forName(driver);
 
         try (Connection connection = DriverManager.getConnection(url, login, password)) {
             DatabaseMetaData metaData = connection.getMetaData();
