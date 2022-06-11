6092a70eecd6f3a709a5ad063abce3427cd3ffb0
Nataliya-nataliya <nataliya.chest@gmail.com>
2022-06-12 00:36:00 +0300

    try with resources and add ClassLoader

--- src/main/java/ru/job4j/jdbc/TableEditor.java
+++ src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -1,8 +1,9 @@
 package ru.job4j.jdbc;
 
-import java.io.FileInputStream;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
 import java.util.StringJoiner;
@@ -26,57 +27,61 @@ public class TableEditor implements AutoCloseable {
         }
     }
 
-    private static Connection getConnection() throws Exception {
+    private static Connection getConnection() throws SQLException {
         String driver = properties.getProperty("hibernate.connection.driver_class");
-        Class.forName(driver);
+        try {
+            Class.forName(driver);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
         String url = properties.getProperty("hibernate.connection.url");
         String login = properties.getProperty("hibernate.connection.username");
         String password = properties.getProperty("hibernate.connection.password");
         return DriverManager.getConnection(url, login, password);
     }
 
-    private void executeQuery(String query) throws Exception {
-        try (Connection connection = getConnection()) {
-            try (Statement statement = connection.createStatement()) {
-                statement.execute(query);
-            }
+    private void executeQuery(String query) {
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(query);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
         }
     }
 
-    public void createTable(String tableName) throws Exception {
-            String sql = String.format(
-                    "CREATE TABLE if not exists %s ();",
-                    tableName
-            );
-            executeQuery(sql);
+    public void createTable(String tableName) {
+        String sql = String.format(
+                "CREATE TABLE if not exists %s ();",
+                tableName
+        );
+        executeQuery(sql);
     }
 
-    public void dropTable(String tableName) throws Exception {
-            String sql = String.format("DROP TABLE if exists %s;",
-                    tableName
-            );
-            executeQuery(sql);
+    public void dropTable(String tableName) {
+        String sql = String.format("DROP TABLE if exists %s;",
+                tableName
+        );
+        executeQuery(sql);
     }
 
-    public void addColumn(String tableName, String columnName, String type) throws Exception {
-            String sql = String.format("ALTER TABLE if exists %s ADD  if not exists %s %s;",
-                    tableName, columnName, type
-            );
-            executeQuery(sql);
+    public void addColumn(String tableName, String columnName, String type) {
+        String sql = String.format("ALTER TABLE if exists %s ADD  if not exists %s %s;",
+                tableName, columnName, type
+        );
+        executeQuery(sql);
     }
 
-    public void dropColumn(String tableName, String columnName) throws Exception {
-            String sql = String.format("ALTER TABLE if exists %s DROP %s;",
-                    tableName, columnName
-            );
-            executeQuery(sql);
+    public void dropColumn(String tableName, String columnName) {
+        String sql = String.format("ALTER TABLE if exists %s DROP %s;",
+                tableName, columnName
+        );
+        executeQuery(sql);
     }
 
-    public void renameColumn(String tableName, String columnName, String newColumnName) throws Exception {
-            String sql = String.format("ALTER TABLE if exists %s RENAME COLUMN %s TO %s;",
-                    tableName, columnName, newColumnName
-            );
-            executeQuery(sql);
+    public void renameColumn(String tableName, String columnName, String newColumnName) {
+        String sql = String.format("ALTER TABLE if exists %s RENAME COLUMN %s TO %s;",
+                tableName, columnName, newColumnName
+        );
+        executeQuery(sql);
     }
 
 
@@ -106,26 +111,29 @@ public class TableEditor implements AutoCloseable {
         }
     }
 
-    public static void main(String[] args) throws Exception {
-        FileInputStream in = new FileInputStream("app.properties");
-        Properties property = new Properties();
-        property.load(in);
-        TableEditor tableEditor = new TableEditor(property);
-        System.out.println("Создание таблицы");
-        tableEditor.createTable("book_store");
-        System.out.println(getTableScheme(tableEditor.connection, "book_store"));
-        System.out.println("Добавление столбцов");
-        tableEditor.addColumn("book_store", "author", "text");
-        tableEditor.addColumn("book_store", "title", "text");
-        tableEditor.addColumn("book_store", "amount", "int");
-        System.out.println(getTableScheme(getConnection(), "book_store"));
-        System.out.println("Переименование столбца");
-        tableEditor.renameColumn("book_store", "author", "authors");
-        System.out.println(getTableScheme(getConnection(), "book_store"));
-        System.out.println("Удаление столбца");
-        tableEditor.dropColumn("book_store", "amount");
-        System.out.println(getTableScheme(getConnection(), "book_store"));
-        System.out.println("Удаление таблицы");
-        tableEditor.dropTable("book_store");
+    public static void main(String[] args) {
+        Properties config = new Properties();
+        try (InputStream in = TableEditor.class.getClassLoader().getResourceAsStream("app.properties")) {
+            config.load(in);
+            TableEditor tableEditor = new TableEditor(config);
+            System.out.println("Создание таблицы");
+            tableEditor.createTable("book_store");
+            System.out.println(getTableScheme(tableEditor.connection, "book_store"));
+            System.out.println("Добавление столбцов");
+            tableEditor.addColumn("book_store", "author", "text");
+            tableEditor.addColumn("book_store", "title", "text");
+            tableEditor.addColumn("book_store", "amount", "int");
+            System.out.println(getTableScheme(getConnection(), "book_store"));
+            System.out.println("Переименование столбца");
+            tableEditor.renameColumn("book_store", "author", "authors");
+            System.out.println(getTableScheme(getConnection(), "book_store7"));
+            System.out.println("Удаление столбца");
+            tableEditor.dropColumn("book_store", "amount");
+            System.out.println(getTableScheme(getConnection(), "book_store"));
+            System.out.println("Удаление таблицы");
+            tableEditor.dropTable("book_store");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
